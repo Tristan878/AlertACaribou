38a54d83972d0eea6a586125939f64854f40def0
Tristan878 <tristan.chatain@etu.unilim.fr>
2023-04-13 01:14:13 +0200

    set up project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from "./main-page/main-page.component";
+import {ListeComponent} from "./liste/liste.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',   redirectTo: '/', pathMatch: 'full' },
+  { path: '', component: MainPageComponent },
+  { path: 'liste', component: ListeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import {NgOptimizedImage} from "@angular/common";
+import { MainPageComponent } from './main-page/main-page.component';
+import { FooterComponent } from './footer/footer.component';
+import { PlanningComponent } from './planning/planning.component';
+import { ListeComponent } from './liste/liste.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    MainPageComponent,
+    FooterComponent,
+    PlanningComponent,
+    ListeComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        NgOptimizedImage
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
