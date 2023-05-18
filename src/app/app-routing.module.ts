40255043581fb61734d0201a43d40ee3b10ca857
Tristan878 <tristan.chatain@etu.unilim.fr>
2023-05-18 23:52:55 +0200

    add roads, fix size on main page, and change pictures

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainPageComponent } from "./main-page/main-page.component";
 import {ListeComponent} from "./liste/liste.component";
+import {PartenariatsComponent} from "./partenariats/partenariats.component";
+import {CookiePopupComponent} from "./cookie-popup/cookie-popup.component";
 
 const routes: Routes = [
   { path: '',   redirectTo: '/', pathMatch: 'full' },
   { path: '', component: MainPageComponent },
   { path: 'liste', component: ListeComponent },
+  { path: 'partenariats', component: PartenariatsComponent },
+  { path: 'cookies', component: CookiePopupComponent }
 ];
 
 @NgModule({
