4634925d0b662f98881c310aad0eedd74645057f
Elisa Gatto <elisa.gatto@live.fr>
2023-05-21 13:27:40 +0200

    modif carrousel - liste - cookies

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainPageComponent } from "./main-page/main-page.component";
 import {ListeComponent} from "./liste/liste.component";
 import {PartenariatsComponent} from "./partenariats/partenariats.component";
-import {CookiePopupComponent} from "./cookie-popup/cookie-popup.component";
+import {PlanningComponent} from "./planning/planning.component";
+import {CookiesComponent} from "./cookies/cookies.component";
 
 const routes: Routes = [
   { path: '',   redirectTo: '/', pathMatch: 'full' },
   { path: '', component: MainPageComponent },
   { path: 'liste', component: ListeComponent },
   { path: 'partenariats', component: PartenariatsComponent },
-  { path: 'cookies', component: CookiePopupComponent }
+  { path: 'planning', component: PlanningComponent },
+  { path: 'cookies', component: CookiesComponent }
 ];
 
 @NgModule({
